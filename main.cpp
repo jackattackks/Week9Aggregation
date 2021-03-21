cffaa70ed5edcfb8e5122c2240212c756eb066e0
Jackie Ocaña <45646092+jackattackks@users.noreply.github.com>
2021-03-21 13:27:11 -0700

    first commit
    player class created

---
+++ main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  Week9Aggregation
+//
+//  Created by Jackie on 3/21/21.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Player class created
+class Player{
+private:
+    string name;
+
+public:
+    string getName(){
+        return name;
+    }
+    
+    void setName(string n){
+        name = n;
+    }
+    
+    void defaultCon(){
+        name = "Unknown";
+    }
+    
+    void parmCons(string list){
+//        list = array name
+    }
+};
+
+//Team class created
+class Team{
+private:
+public:
+    
+};
+
+int main() {
+    
+}
